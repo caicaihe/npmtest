76927d8c49b2e262706ab23094158c2741a26929
Liping He <43132214+caicaihe@users.noreply.github.com>
2018-10-18 10:10:34 +0800

    Update cc.js

--- cc.js
+++ cc.js
@@ -12,3 +12,74 @@ app.get('/', function (req, res) {
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
+
+'use strict';
+
+var express = require('express');
+
+var PORT = 8888;
+
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
+
+'use strict';
+
+var express = require('express');
+
+var PORT = 8888;
+
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
+
+'use strict';
+
+var express = require('express');
+
+var PORT = 8888;
+
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
+
+'use strict';
+
+var express = require('express');
+
+var PORT = 8888;
+
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
+
+
+'use strict';
+
+var express = require('express');
+
+var PORT = 8888;
+
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
